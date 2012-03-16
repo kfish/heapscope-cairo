59ef1ed020739a1ad4a9b387fa2bd3b27466417b
Conrad Parker <conrad@metadecks.org>
2012-03-16 12:17:06 +0800

    initial commit: based on GUI.hs from scope-cairo

---
+++ src/GUI.hs
@@ -0,0 +1,162 @@
+{-# LANGUAGE RecordWildCards #-}
+{-# OPTIONS -Wall -fno-warn-unused-do-bind #-}
+
+module Main (
+    main
+) where
+
+import Control.Applicative ((<$>))
+import Control.Monad.Reader
+import Data.IORef
+import Data.Maybe
+import qualified Graphics.UI.Gtk as G
+import System.Environment (getArgs)
+
+import Paths_heapscope_cairo as My
+import Scope.Layer
+import Scope.Types
+
+import Scope.Cairo
+
+----------------------------------------------------------------------
+
+windowWidth, windowHeight :: Int
+windowWidth   = 500
+windowHeight  = 500
+
+main :: IO ()
+main = do
+  _ <- G.initGUI
+
+  args <- getArgs
+
+  window <- G.windowNew
+  G.widgetSetSizeRequest window windowWidth windowHeight
+  G.widgetSetAppPaintable window True
+  G.widgetSetDoubleBuffered window True
+
+  vbox <- G.vBoxNew False 0
+  G.containerAdd window vbox
+
+  ui <- G.uiManagerNew
+
+  filename <- My.getDataFileName "data/actions.ui"
+  G.uiManagerAddUiFromFile ui filename
+
+  let getWidget = fmap fromJust . G.uiManagerGetWidget ui
+
+  -- Menubar
+  fma <- G.actionNew "FMA" "File" Nothing Nothing
+  ema <- G.actionNew "EMA" "Edit" Nothing Nothing
+  vma <- G.actionNew "VMA" "View" Nothing Nothing
+  hma <- G.actionNew "HMA" "Help" Nothing Nothing
+
+  -- File menu
+  newa <- G.actionNew "NEWA" "New" (Just "Just a Stub") (Just G.stockNew)
+  newa `G.on` G.actionActivated $ myNew
+  opena <- G.actionNew "OPENA" "Open" (Just "Just a Stub") (Just G.stockOpen)
+  savea <- G.actionNew "SAVEA" "Save" (Just "Just a Stub") (Just G.stockSave)
+  saveasa <- G.actionNew "SAVEASA" "Save As" (Just "Just a Stub") (Just G.stockSaveAs)
+  quita <- G.actionNew "QUITA" "Quit" (Just "Just a Stub") (Just G.stockQuit)
+
+  let fChooser action label = G.fileChooserDialogNew Nothing (Just window) action
+          [(G.stockCancel, G.ResponseCancel), (label, G.ResponseAccept)]
+
+  openDialog <- fChooser G.FileChooserActionOpen G.stockOpen
+  demoPath <- My.getDataFileName "demo"
+  G.fileChooserSetCurrentFolder openDialog demoPath
+
+  opena `G.on` G.actionActivated $ G.widgetShow openDialog
+
+  saveDialog <- fChooser G.FileChooserActionSave G.stockSave
+  savea `G.on` G.actionActivated $ G.widgetShow saveDialog
+  saveasa `G.on` G.actionActivated $ G.widgetShow saveDialog
+
+  -- Edit menu
+  cut1 <- G.actionNew "cut1" "Cut" (Just "Just a Stub") (Just G.stockCut)
+  cut1 `G.on` G.actionActivated $ myCut
+  copy1 <- G.actionNew "copy1" "Copy" (Just "Just a Stub") (Just G.stockCopy)
+  copy1 `G.on` G.actionActivated $ myCopy
+  paste1 <- G.actionNew "paste1" "Paste" (Just "Just a Stub") (Just G.stockPaste)
+  paste1 `G.on` G.actionActivated $ myPaste
+  delete1 <- G.actionNew "delete1" "Delete" (Just "Just a Stub") (Just G.stockDelete)
+  delete1 `G.on` G.actionActivated $ myDelete
+
+  -- Help menu
+  -- About dialog
+  aboutdialog <- G.aboutDialogNew
+  abouta <- G.actionNew "ABOUTA" "About" (Just "Just a Stub") Nothing
+  abouta `G.on` G.actionActivated $ G.widgetShow aboutdialog
+  aboutdialog `G.on` G.response $ const $ G.widgetHide aboutdialog
+
+  -- Action group
+  agr <- G.actionGroupNew "AGR"
+  mapM_ (G.actionGroupAddAction agr) [fma, ema, vma, hma]
+  mapM_ (\act -> G.actionGroupAddActionWithAccel agr act Nothing)
+      [ newa, opena, savea, saveasa, quita
+      , cut1, copy1, paste1, delete1
+      , abouta
+      ]
+
+  G.uiManagerInsertActionGroup ui agr 0
+
+  menubar <- getWidget "/ui/menubar1"
+  G.boxPackStart vbox menubar G.PackNatural 0
+
+  scopeRef <- scopeCairoNew
+  ViewCairo{..} <- viewUI . view <$> readIORef scopeRef
+
+  quita `G.on` G.actionActivated $ myQuit scopeRef window
+
+  mapM_ (modifyIORefM scopeRef . addLayersFromFile) args
+  openDialog `G.on` G.response $ myFileOpen scopeRef openDialog
+  saveDialog `G.on` G.response $ myFileSave scopeRef saveDialog
+
+  G.boxPackStart vbox frame G.PackGrow 0
+  scopeCairoDefaultEvents scopeRef
+
+  statusbar <- G.statusbarNew
+  G.boxPackStart vbox statusbar G.PackNatural 0
+
+  G.onDestroy window G.mainQuit
+
+  G.widgetShowAll window
+  G.mainGUI
+
+myQuit :: G.WidgetClass cls => IORef (Scope ViewCairo) -> cls -> IO ()
+myQuit scopeRef window = do
+  scopeModifyMUpdate scopeRef scopeClose
+  G.widgetDestroy window
+
+myNew :: IO ()
+myNew = putStrLn "New"
+
+myFileOpen :: IORef (Scope ViewCairo) -> G.FileChooserDialog -> G.ResponseId -> IO ()
+myFileOpen scopeRef fcdialog response = do
+  case response of
+    G.ResponseAccept -> do
+        Just filename <- G.fileChooserGetFilename fcdialog
+        scopeModifyMUpdate scopeRef (addLayersFromFile filename)
+    _ -> return ()
+  G.widgetHide fcdialog
+
+myFileSave :: IORef (Scope ViewCairo) -> G.FileChooserDialog -> G.ResponseId -> IO ()
+myFileSave scopeRef fcdialog response = do
+  case response of
+    G.ResponseAccept -> do
+        Just filename <- G.fileChooserGetFilename fcdialog
+        writePng filename scopeRef
+    _ -> return ()
+  G.widgetHide fcdialog
+
+myCut :: IO ()
+myCut = putStrLn "Cut"
+
+myCopy :: IO ()
+myCopy = putStrLn "Copy"
+
+myPaste :: IO ()
+myPaste = putStrLn "Paste"
+
+myDelete :: IO ()
+myDelete = putStrLn "Delete"
